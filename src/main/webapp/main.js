2499b709968dbf65047e0548ad8f87f7f4664e87
ximbong91023 <ximbong91023@users.noreply.github.com>
2018-05-07 21:18:09 +0300

    Clean up some code and add 2 fetch calls.
    Fetch calls for posting city names and getting the recommendation list.

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -1,59 +1,38 @@
 $(document).ready(function() {
 
+  let url = "https://travelie.herokuapp.com/";
+
+  fetch(url, {
+      method: "GET",
+    }).then(response => response.json())
+    .then(response => console.log(response))
+
+  function postCityName(name) {
+    fetch(url, {
+      method: "POST",
+      body: name
+    })
+    window.location = "result.html";
+    localStorage.setItem("cityName", name);
+  }
   $(".cityName").click(function() {
     let cityName = $(this).text();
-    let  url = "/cityRumble?name="+cityName;
-    fetch(url, {
-    method: "POST",
-    cache: 'no-cache'
-    })
-    .then(response => response.text())
-	.then (response => console.log(response))
-	.then(function(result){console.log(result)});
-
-    console.log(cityName);
-    localStorage.setItem("cityName", cityName);
-    window.location = "result.html";
+    postCityName(cityName);
   });
 
   $(".readMore").click(function() {
     let cityName = $(this).parent().find(".cityName").text();
+    postCityName(cityName);
+  });
 
-    let  url = "/cityRumble?name="+cityName;
-    fetch(url, {
-      method: "POST",
-      cache: 'no-cache'
+  $('.citySearch > input').keypress(function(e) {
+    if (e.which == 13) {
+      let cityName = $(this).val();
+      postCityName(cityName);
     }
-    )
-    .then(response => response.text())
-	.then (response => console.log(response))
-	.then(function(result){console.log(result)});
-
-    console.log(cityName);
-    window.location = "result.html";
-    localStorage.setItem("cityName", cityName);
   });
 
   $(".logoDiv").click(function() {
     window.location = "index.html";
-  });
-
-  $('.citySearch > input').keypress(function(e) {
-      if (e.which == 13) {
-        let cityName = $(this).val();
-        let  url = "/cityRumble?name="+cityName;
-
-        fetch(url, {
-        method: "POST",
-        cache: 'no-cache'
-        })
-       .then(response => response.text())
-	   .then (response => console.log(response))
-	   .then(function(result){console.log(result)});
-
-        console.log(cityName)
-        localStorage.setItem("cityName", cityName);
-        window.location = "result.html";
-      }
   });
 });
