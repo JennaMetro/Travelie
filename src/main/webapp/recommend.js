a25c1537032b7dec237a7606a06ea1bfa983d1b4
ximbong91023 <ximbong91023@users.noreply.github.com>
2018-05-07 23:59:02 +0300

    Fix fetch call for images in place recommendation

--- src/main/webapp/recommend.js
+++ src/main/webapp/recommend.js
@@ -1,22 +1,23 @@
 $(document).ready(function() {
 
-  let url = "https://travelie.herokuapp.com/city";
+  let url = "https://cors-anywhere.herokuapp.com/https://travelie.herokuapp.com/city";
 
 
   fetch(url, {
       method: "GET",
     }).then(response => response.json())
     .then(function(response) {
-      for (value of response.results) {
+      for (let value of response.results) {
         let name = value.name;
-        let url2 = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=things+to+do+in${name}&key=AIzaSyA51GtAToTOjiu4pEYW8miXs6lwexTMZvk`;
-
-        fetch(url, {
+        let url2 = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=things+to+do+in+${name}&key=AIzaSyA51GtAToTOjiu4pEYW8miXs6lwexTMZvk`;
+        fetch(url2, {
             method: "GET",
           })
           .then(response => response.json())
           .then(function(response) {
             let element = response.results[0];
+            console.log(element)
+            let photo_ref;
             if (element.photos === undefined) {
               photo_ref = `CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU`;
             } else photo_ref = element.photos[0].photo_reference;
