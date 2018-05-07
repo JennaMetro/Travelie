105bfa9e54ec959554cce63c7f185a79b6fef2e6
Cuong Phan <phthcuong19@gmail.com>
2018-05-08 00:48:15 +0300

    add algorithm... with some bugs

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -7,8 +7,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
-
     public static void main(String[] args) {
+        Model.cityList.put("Helsinki", 10);
+        Model.cityList.put("Singapore", 9);
+        Model.cityList.put("Hong Kong", 8);
+        Model.cityList.put("New York", 7);
+        Model.cityList.put("Rome", 6);
+        Model.cityList.put("Los Angeles", 5);
+        Model.cityList.put("Seoul", 4);
+        Model.cityList.put("Athens", 3);
+        Model.cityList.put("Tokyo", 2);
+        Model.cityList.put("Kyoto", 1);
+        Model.mostSearchedCities.put("Helsinki", 10);
+        Model.mostSearchedCities.put("Singapore", 9);
+        Model.mostSearchedCities.put("Hong Kong", 8);
+        Model.mostSearchedCities.put("New York", 7);
+        Model.mostSearchedCities.put("Rome", 6);
+        Model.mostSearchedCities.put("Los Angeles", 5);
+        Model.mostSearchedCities.put("Seoul", 4);
+        Model.mostSearchedCities.put("Athens", 3);
+        Model.mostSearchedCities.put("Tokyo", 2);
+        Model.mostSearchedCities.put("Kyoto", 1);
         SpringApplication.run(App.class, args);
     }
 }
--- src/main/java/app/Controller.java
+++ src/main/java/app/Controller.java
@@ -38,61 +38,96 @@ public class Controller {
     @RequestMapping(value = "/city", method = RequestMethod.GET)
     @SuppressWarnings("unchecked")
     public JSONObject test() {
-        JSONArray ja = new JSONArray();
-        JSONObject obj = new JSONObject();
-        obj.put("name", "Helsinki");
-        ja.add(obj);
-        obj = new JSONObject();
-        obj.put("name", "Singapore");
-        ja.add(obj);
-        obj = new JSONObject();
-        obj.put("name", "Hong Kong");
-        ja.add(obj);
-        obj = new JSONObject();
-        obj.put("name", "New York");
-        ja.add(obj);
-        obj = new JSONObject();
-        obj.put("name", "Rome");
-        ja.add(obj);
-        obj = new JSONObject();
-        obj.put("name", "Los Angeles");
-        ja.add(obj);
-        obj = new JSONObject();
-        obj.put("name", "Seoul");
-        ja.add(obj);
-        obj = new JSONObject();
-        obj.put("name", "Athens");
-        ja.add(obj);
-        obj = new JSONObject();
-        obj.put("name", "Tokyo");
-        ja.add(obj);
-        obj = new JSONObject();
-        obj.put("name", "Kyoto");
-        ja.add(obj);
-        
         JSONObject results = new JSONObject();
+
+//        if (Model.mostSearchedCities.size() < 10) {
+//            JSONArray ja = new JSONArray();
+//            JSONObject obj = new JSONObject();
+//            obj.put("name", "Helsinki");
+//            ja.add(obj);
+//            obj = new JSONObject();
+//            obj.put("name", "Singapore");
+//            ja.add(obj);
+//            obj = new JSONObject();
+//            obj.put("name", "Hong Kong");
+//            ja.add(obj);
+//            obj = new JSONObject();
+//            obj.put("name", "New York");
+//            ja.add(obj);
+//            obj = new JSONObject();
+//            obj.put("name", "Rome");
+//            ja.add(obj);
+//            obj = new JSONObject();
+//            obj.put("name", "Los Angeles");
+//            ja.add(obj);
+//            obj = new JSONObject();
+//            obj.put("name", "Seoul");
+//            ja.add(obj);
+//            obj = new JSONObject();
+//            obj.put("name", "Athens");
+//            ja.add(obj);
+//            obj = new JSONObject();
+//            obj.put("name", "Tokyo");
+//            ja.add(obj);
+//            obj = new JSONObject();
+//            obj.put("name", "Kyoto");
+//            ja.add(obj);
+//            results.put("results", ja);
+//        } else {
+//            JSONArray ja = new JSONArray();
+//            JSONObject obj;
+//            for (String s : Model.mostSearchedCities.keySet()) {
+//                obj = new JSONObject();
+//                obj.put("name", s);
+//                ja.add(obj);
+//            }
+//            results.put("results", ja);
+//        }
+        JSONArray ja = new JSONArray();
+        JSONObject obj;
+        for (String s : Model.mostSearchedCities.keySet()) {
+            obj = new JSONObject();
+            obj.put("name", s);
+            ja.add(obj);
+        }
         results.put("results", ja);
-        
+
         return results;
     }
 
     @RequestMapping(value = "/city", method = RequestMethod.POST)
     public String cityNameSearched(@RequestBody String cityName) {
+        System.out.println(Model.mostSearchedCities.size());
+        System.out.println("-------------");
         if (Model.cityList.containsKey(cityName)) {
             int newCount = Model.cityList.get(cityName) + 1;
             Model.cityList.put(cityName, newCount);
-            
-            int smallest = Integer.MAX_VALUE ;
-            for(Map.Entry<String, Integer> entry : Model.mostSearchedCities.entrySet()) {
+
+            int smallest = Integer.MAX_VALUE;
+            for (Map.Entry<String, Integer> entry : Model.mostSearchedCities.entrySet()) {
                 if (smallest > entry.getValue()) {
                     smallest = entry.getValue();
                 }
             }
-        }
-        else {
+
+            if (newCount > smallest) {
+                if (Model.mostSearchedCities.size() >= 10) {
+                    for (String s : Model.mostSearchedCities.keySet()) {
+                        if (Model.mostSearchedCities.get(s) == smallest) {
+                            if(Model.mostSearchedCities.size() >= 10) {
+                                Model.mostSearchedCities.remove(s);
+                            }                           
+                            break;
+                        }
+                    }
+                }
+
+                Model.mostSearchedCities.put(cityName, newCount);
+            }
+        } else {
             Model.cityList.put(cityName, 1);
         }
-        
+
         return "successfully added";
     }
 }
